7de7b4e31582bb610508678bb91ac1ec33c97b0e
lopezkarylle <dr.lopezkarylle@gmail.com>
2023-04-26 19:25:32 +0800

    first commit

---
+++ Module5Exercise1/Module5Exercise1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module5Exercise1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnDateSelected(object sender, DateChangedEventArgs args)
+        {
+            Recalculate();
+        }
+
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            Recalculate();
+        }
+
+        void Recalculate()
+        {
+            TimeSpan timeSpan = endDate.Date - startDate.Date +
+                (toggleSwitch.IsToggled ? TimeSpan.FromDays(1) : TimeSpan.Zero);
+
+            resultLabel.Text = String.Format("You have {0} day{1} left",
+                                                timeSpan.Days, timeSpan.Days == 1 ? "" : "s");
+        }
+
+    }
+}
